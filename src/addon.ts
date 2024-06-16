5c6eb3d44a23e7c2070702b7f23cc23e274fafa1
Sleeyax <yourd3veloper@gmail.com>
2024-06-16 18:45:37 +0200

    Add basic error handling

--- src/addon.ts
+++ src/addon.ts
@@ -32,26 +32,31 @@ export async function catalogHandler({
     console.log(id, extra, api);
   }
 
-  const [, contentType, provider] = id.split('-');
-
-  let metas: MetaPreview[] = [];
-
-  const consumetApi = new ConsumetApi(api);
-
-  const searchResults = await consumetApi.search(
-    contentType as ConsumetContentType,
-    provider as AnimeProvider,
-    extra.search
-  );
-  metas = (searchResults ?? []).map((searchResult) => ({
-    id: `${ADDON_ID}:${contentType}:${provider}:${searchResult.id}`,
-    name: searchResult.title.toString(),
-    type: 'series',
-    poster: searchResult.image,
-    posterShape: 'regular',
-  }));
-
-  return { metas };
+  try {
+    const [, contentType, provider] = id.split('-');
+  
+    let metas: MetaPreview[] = [];
+  
+    const consumetApi = new ConsumetApi(api);
+  
+    const searchResults = await consumetApi.search(
+      contentType as ConsumetContentType,
+      provider as AnimeProvider,
+      extra.search
+    );
+    metas = (searchResults ?? []).map((searchResult) => ({
+      id: `${ADDON_ID}:${contentType}:${provider}:${searchResult.id}`,
+      name: searchResult.title.toString(),
+      type: 'series',
+      poster: searchResult.image,
+      posterShape: 'regular',
+    }));
+  
+    return { metas };
+  } catch (e) {
+    console.error(e);
+    return { metas: [] };
+  }
 }
 
 export async function metaHandler({
@@ -67,52 +72,57 @@ export async function metaHandler({
     console.log(id, type);
   }
 
-  const [, contentType, provider, consumetId] = id.split(':');
+  try {
+    const [, contentType, provider, consumetId] = id.split(':');
 
-  let meta: MetaDetail = null as unknown as MetaDetail;
+    let meta: MetaDetail = null as unknown as MetaDetail;
 
-  const consumetApi = new ConsumetApi(api);
+    const consumetApi = new ConsumetApi(api);
 
-  const info = await consumetApi.getInfo(
-    contentType as ConsumetContentType,
-    provider as AnimeProvider,
-    consumetId
-  );
+    const info = await consumetApi.getInfo(
+      contentType as ConsumetContentType,
+      provider as AnimeProvider,
+      consumetId
+    );
 
-  if (info != null) {
-    meta = {
-      id: `${ADDON_ID}:${contentType}:${provider}:${info.id}`,
-      name: info.title.toString(),
-      type,
-      background: info.cover,
-      logo: info.image,
-      country: info.countryOfOrigin,
-      description: info.description,
-      genres: info.genres,
-      imdbRating: info.rating?.toString(),
-      released: info.releaseDate
-        ? new Date(info.releaseDate).toISOString()
-        : undefined,
-      releaseInfo:
-        info.startDate || info.endDate
-          ? `${info.startDate ? formatFuzzyDate(info.startDate) : ''} - ${info.endDate ? formatFuzzyDate(info.endDate) : ''}`
+    if (info != null) {
+      meta = {
+        id: `${ADDON_ID}:${contentType}:${provider}:${info.id}`,
+        name: info.title.toString(),
+        type,
+        background: info.cover,
+        logo: info.image,
+        country: info.countryOfOrigin,
+        description: info.description,
+        genres: info.genres,
+        imdbRating: info.rating?.toString(),
+        released: info.releaseDate
+          ? new Date(info.releaseDate).toISOString()
           : undefined,
-      website: info.url,
-      runtime: info.status,
-      videos: info.episodes?.map((episode) => ({
-        id: `${ADDON_ID}:${contentType}:${provider}:${episode.id}`,
-        released: new Date(
-          episode.releaseDate || info.releaseDate || 1970
-        ).toISOString(),
-        title: episode.title?.toString() || `Episode ${episode.number}`,
-        episode: episode.number,
-        overview: episode.description,
-        season: 1,
-      })),
-    };
-  }
+        releaseInfo:
+          info.startDate || info.endDate
+            ? `${info.startDate ? formatFuzzyDate(info.startDate) : ''} - ${info.endDate ? formatFuzzyDate(info.endDate) : ''}`
+            : undefined,
+        website: info.url,
+        runtime: info.status,
+        videos: info.episodes?.map((episode) => ({
+          id: `${ADDON_ID}:${contentType}:${provider}:${episode.id}`,
+          released: new Date(
+            episode.releaseDate || info.releaseDate || 1970
+          ).toISOString(),
+          title: episode.title?.toString() || `Episode ${episode.number}`,
+          episode: episode.number,
+          overview: episode.description,
+          season: 1,
+        })),
+      };
+    }
 
-  return { meta };
+    return { meta };
+  } catch (e) {
+    console.error(e);
+    return { meta: null as unknown as MetaDetail };
+  }
 }
 
 export async function streamHandler({
@@ -128,26 +138,31 @@ export async function streamHandler({
     console.log(id, type);
   }
 
-  const [, contentType, provider, episodeId] = id.split(':');
-
-  let streams: Stream[] = [];
-
-  const consumetApi = new ConsumetApi(api);
-
-  const source = await consumetApi.getEpisodeSources(
-    contentType as ConsumetContentType,
-    provider as AnimeProvider,
-    episodeId
-  );
-  streams = (source?.sources ?? []).map((s) => ({
-    name: s.quality ?? 'Unknown',
-    title: s.quality ?? 'Unknown',
-    subtitles: source?.subtitles as Subtitle[],
-    url: s.url,
-    behaviorHints: {
-      headers: source?.headers,
-    },
-  }));
-
-  return { streams };
+  try {
+    const [, contentType, provider, episodeId] = id.split(':');
+  
+    let streams: Stream[] = [];
+  
+    const consumetApi = new ConsumetApi(api);
+  
+    const source = await consumetApi.getEpisodeSources(
+      contentType as ConsumetContentType,
+      provider as AnimeProvider,
+      episodeId
+    );
+    streams = (source?.sources ?? []).map((s) => ({
+      name: s.quality ?? 'Unknown',
+      title: s.quality ?? 'Unknown',
+      subtitles: source?.subtitles as Subtitle[],
+      url: s.url,
+      behaviorHints: {
+        headers: source?.headers,
+      },
+    }));
+  
+    return { streams };
+  } catch (e) {
+    console.error(e);
+    return { streams: [] };
+  }
 }
